8ba8d97af967970bbd4a14cbdfb58a3f37d5d7de
Mixuttih <alanhommis@gmail.com>
2025-09-24 12:16:28 +0300

    kommentteja lisätty

---
+++ kysymykset.py
@@ -0,0 +1,15 @@
+# Whitch country is airport x located?
+
+# How high is x airport?
+
+# What is a distance between x and x airport?
+
+# How much CO2 emissions does flight from x airport to x airport produce?
+
+# What is x airport's gps-code?
+
+# Which is highest airport from sea level?
+
+# Which is lowest airport from sea level?
+
+# Which airport is most uneconomical airport you can fly to from helsinki-vantaa airport?
