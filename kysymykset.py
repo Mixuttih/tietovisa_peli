a374a94782cda1c411114e3ff40901cda36190b3
eetua <elsavii@metropolia.fi>
2025-09-28 22:15:15 +0300

    What is x airport's gps-code?

--- kysymykset.py
+++ kysymykset.py
@@ -8,6 +8,12 @@ sql3 = f"SELECT name FROM country WHERE NOT iso_country = '{oikea_vastaus[1]}' O
 sql4 = f"SELECT name FROM airport ORDER BY RAND() LIMIT 1"
 sql5 = f"SELECT elevation_ft*0.3048 as elevation_m FROM airport WHERE name = '{kysymys[2]}'"
 sql6 = f"SELECT elevation_ft*0.3048 as elevation_m FROM airport WHERE NOT name = '{oikea_vastaus[2]' ORDER BY RAND() LIMIT 3"
+
+# What is x airport's gps-code?
+sql7 = f"SELECT name FROM airport ORDER BY RAND() LIMIT 1"
+sql8 = f"SELECT gps_code FROM airport WHERE name = '{kysymys[3]}'"
+sql9 = f"SELECT gps_code FROM airport WHERE NOT name = '{oikea_vastaus[3]' ORDER BY RAND() LIMIT 3"
+
 # What is a distance between x and x airport?
 
 # get airport info
@@ -30,7 +36,6 @@ def calculate_distance(current, target):
 
 # How much CO2 emissions does flight from x airport to x airport produce?
 
-# What is x airport's gps-code?
 
 # Which is highest airport from sea level?
 
