6ab3891fd93aad371937c78404d92f7ad845752a
eetuviirret <elsavii@metropolia.fi>
2025-09-29 18:43:53 +0300

    mitä tein aamulla

--- kysymykset.py
+++ kysymykset.py
@@ -1,27 +1,43 @@
+import random
+import mysql.connector
+import tarina
+
+
+#SQL yhteys
+yhteys = mysql.connector.connect(
+    host='localhost',
+    port=3307,
+    database='flight_game',
+    user='root',
+    password='Sorsalampi2025',
+    autocommit=True
+)
 
 # Which country is airport x located?
 sql1 = f"SELECT name, ident FROM airport ORDER BY RAND() LIMIT 1"
-sql2 = f"SELECT name, iso_country FROM country WHERE iso_country in(SELECT iso_country FROM airport WHERE ident = '{kysymys[1]}')"
-sql3 = f"SELECT name FROM country WHERE NOT iso_country = '{oikea_vastaus[1]}' ORDER BY RAND() LIMIT 3"
+sql2 = f"SELECT name, iso_country FROM country WHERE iso_country in(SELECT iso_country FROM airport WHERE ident = '{sql1}')"
+sql3 = f"SELECT name FROM country WHERE NOT iso_country = '{sql2}' ORDER BY RAND() LIMIT 3"
 
 # How high is x airport?
-sql4 = f"SELECT name FROM airport ORDER BY RAND() LIMIT 1"
-sql5 = f"SELECT elevation_ft*0.3048 as elevation_m FROM airport WHERE name = '{kysymys[2]}'"
-sql6 = f"SELECT elevation_ft*0.3048 as elevation_m FROM airport WHERE NOT name = '{oikea_vastaus[2]' ORDER BY RAND() LIMIT 3"
+sql4 = f"SELECT name, ident FROM airport ORDER BY RAND() LIMIT 1"
+sql5 = f"SELECT elevation_ft*0.3048 as elevation_m FROM airport WHERE ident = '{sql4}'"
+sql6 = f"SELECT elevation_ft*0.3048 as elevation_m FROM airport WHERE NOT ident = '{sql5}' ORDER BY RAND() LIMIT 3"
 
 # What is x airport's gps-code?
-sql7 = f"SELECT name FROM airport ORDER BY RAND() LIMIT 1"
-sql8 = f"SELECT gps_code FROM airport WHERE name = '{kysymys[3]}'"
-sql9 = f"SELECT gps_code FROM airport WHERE NOT name = '{oikea_vastaus[3]' ORDER BY RAND() LIMIT 3"
+sql7 = f"SELECT name, ident FROM airport ORDER BY RAND() LIMIT 1"
+sql8 = f"SELECT gps_code FROM airport WHERE ident = '{sql7}'"
+sql9 = f"SELECT gps_code FROM airport WHERE NOT ident = '{sql8}' ORDER BY RAND() LIMIT 3"
 
 # What is a distance between x and x airport?
+sql10 = f"SELECT name, ident, latitude_deg, longitude_deg FROM airport ORDER BY RAND() LIMIT 2"
+sql11 = f"SELECT name ident, latitude_deg, longitude_deg FROM airport ORDER BY RAND() LIMIT 5"
 
 # get airport info
 def get_airport_info(icao):
     sql = f'''SELECT iso_country, ident, name, latitude_deg, longitude_deg
                   FROM airport
                   WHERE ident = %s'''
-    cursor = conn.cursor(dictionary=True)
+    cursor = yhteys.cursor(dictionary=True)
     cursor.execute(sql, (icao,))
     result = cursor.fetchone()
     return result
@@ -32,6 +48,7 @@ def calculate_distance(current, target):
     end = get_airport_info(target)
     return distance.distance((start['latitude_deg'], start['longitude_deg']),
                              (end['latitude_deg'], end['longitude_deg'])).km
+
 
 
 # How much CO2 emissions does flight from x airport to x airport produce?
